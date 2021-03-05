90e09abe865239ab100aaa802a04d7c9c5ba3f64
ap <>
2021-03-05 16:00:52 +0300

    sending transaction only with privatekey, reflected in help

--- metahash_cli_bin.sh
+++ metahash_cli_bin.sh
@@ -23,10 +23,10 @@ usage () {
     echo -e " \ngen-transaction -- gives you binary of transaction.\n\tMandatory parameters:"
     echo -e " \t --amount=AMOUNT_TO_SEND,\n \t --send_to=RECEPIENT_ADDRESS --nonce=VALUE"
     echo -e " \nprepare_transaction -- gives you json of transaction.\n\tMandatory parameters:"
-    echo -e " \t --net=NETWORK(dev|main|test)\n \t --pubkey=/path/to/public_key\n \t --privkey=/path/to/private_key\n \t --amount=AMOUNT_TO_SEND,\n \t --send_to=RECEPIENT_ADDRESS"
+    echo -e " \t --net=NETWORK(dev|main|test)\n \t --pubkey=/path/to/public_key(Optional)\n \t --privkey=/path/to/private_key\n \t --amount=AMOUNT_TO_SEND,\n \t --send_to=RECEPIENT_ADDRESS"
     echo -e "\tOptional parameters: \n \t --nonce=VALUE \n \t --dataHex=DATA_in_HEX"
     echo -e "send_transaction -- send a transaction to server.\n\tMandatory parameters:"
-    echo -e " \t --net=NETWORK(dev|main|test)\n \t --pubkey=/path/to/public_key\n \t --privkey=/path/to/private_key\n \t --amount=AMOUNT_TO_SEND,\n \t --send_to=RECEPIENT_ADDRESS"
+    echo -e " \t --net=NETWORK(dev|main|test)\n \t --pubkey=/path/to/public_key(Optional)\n \t --privkey=/path/to/private_key\n \t --amount=AMOUNT_TO_SEND,\n \t --send_to=RECEPIENT_ADDRESS"
 
     exit 1
 }
